bb105ae64349f1f13dcab7dd188fc2705cf2530b
prnvrvs <71942832+prnvrvs@users.noreply.github.com>
2025-03-07 07:51:37 +0100

    Update test.py

--- example/Trigonal/test.py
+++ example/Trigonal/test.py
@@ -2,8 +2,8 @@ from elastic_vasp import elastic_gen as eg  # This will import module
 from elastic_vasp import constants 
 # This will generate folder of calculation through vasp
 #======= pre-processing ========
-#eg.orthogonal_pre()   # uncoment during preprocessing and comment during post processing
-
+#eg.trigonal_2_pre()   # uncoment during preprocessing and comment during post processing
+#eg.trigonal_1_pre()
 # This will create folder for different distorion and range of strain folder inside as mentioned in strain.dat file
 # copy INCAR POTCAR KPOINTS to each strain-folder inside each folder and do calculation for each strain-folder
 
